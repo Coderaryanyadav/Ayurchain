cece47d40005c1dcc6eb80669676311dd5d8f236
SBMPC Student <SBMPC.Student@SVKMGRP.COM>
2026-09-21 09:58:57 +0530

    Configure deployed smart contract address: 0xd9145CCE52D386f254917e481eB44e9943F39138

--- blockchain/js/contract-config.js
+++ blockchain/js/contract-config.js
@@ -4,7 +4,7 @@
 // File: blockchain/js/contract-config.js
 // ====================================================================
 
-const CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000000";
+const CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138";
 
 const CONTRACT_ABI = [
   {
